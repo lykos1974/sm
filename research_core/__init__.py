58f7c784201999c580d4f47520cd3412fd8a1b24
lykos1974 <vasomihaniki@gmail.com>
2026-07-09 14:08:18 +0300

    Add open interest evidence source (#323)

--- research_core/__init__.py
+++ research_core/__init__.py
@@ -4,6 +4,7 @@ from .enums import DecisionType, ValidationOutcome
 from .evidence import Evidence
 from .evidence_source import EvidenceSource
 from .funding_rate_evidence_source import FundingRateEvidenceSource, FundingRateProvider
+from .open_interest_evidence_source import OpenInterestEvidenceSource, OpenInterestProvider
 from .static_evidence_source import StaticEvidenceSource
 from .exceptions import RequiredFieldError, ResearchCoreError
 from .hypothesis import Hypothesis
@@ -22,6 +23,8 @@ __all__ = (
     "FundingRateProvider",
     "Hypothesis",
     "Knowledge",
+    "OpenInterestEvidenceSource",
+    "OpenInterestProvider",
     "Observation",
     "RequiredFieldError",
     "ResearchCoreError",
